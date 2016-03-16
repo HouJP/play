d88ba77ad3ba7460ab741a4adce432f1df5e6318
HouJP <houjp1992@gmail.com>
2016-03-16 21:52:25 +0800

    update

---
+++ bin/fs_user-visit-count.sh
@@ -0,0 +1,36 @@
+#########################################################################
+# File Name: fs_user-active-count.sh
+# Author: HouJP
+# mail: houjp1992@gmail.com
+# Created Time: Wed Mar 16 20:47:49 2016
+#########################################################################
+#! /bin/bash
+
+PATH_PRE="`pwd`"
+PATH_NOW="`dirname $0`"
+cd "${PATH_NOW}"
+source ../conf/shell.conf
+cd "${PATH_PRE}"
+
+vvd_fp=${HDFS_PROJECT_PT}/data/raw/video-visit-data.txt
+out_fp=${HDFS_PROJECT_PT}/data/fs/user-visit-count.txt
+t_wid=6
+w_len=5
+
+hdfs dfs -rmr $out_fp
+
+class=com.houjp.tianyi.classification.feature.UserVisitCount
+
+spark-submit \
+	--class $class \
+	${LOCAL_JAR_FP} \
+	--vvd_fp ${vvd_fp} \
+	--out_fp ${out_fp} \
+	--t_wid ${t_wid} \
+	--w_len ${w_len}
+
+if [ 0 -eq $? ]; then
+	echo "[INFO] $class success."
+else
+	echo "[ERROR] $class meet error!"
+fi
---
+++ src/main/scala/com/houjp/tianyi/classification/feature/UserVisitCount.scala
@@ -0,0 +1,73 @@
+package com.houjp.tianyi.classification.feature
+
+import com.houjp.tianyi
+import com.houjp.tianyi.classification.FeatureOpts
+import com.houjp.tianyi.datastructure.RawPoint
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkContext, SparkConf}
+import scopt.OptionParser
+
+object UserVisitCount {
+
+  /** command line parameters */
+  case class Params(vvd_fp: String = tianyi.project_pt + "/data/raw/video-visit-data.txt.small",
+                    out_fp: String = tianyi.project_pt + "/data/fs/user-visit-count.txt",
+                    t_wid: Int = 6,
+                    w_len: Int = 5)
+
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("aka").setLevel(Level.WARN)
+    val default_params = Params()
+
+    val parser = new OptionParser[Params]("") {
+      head("", "1.0")
+      opt[String]("vvd_fp")
+        .text("")
+        .action { (x, c) => c.copy(vvd_fp = x) }
+      opt[String]("out_fp")
+        .text("")
+        .action { (x, c) => c.copy(out_fp = x) }
+      opt[Int]("t_wid")
+        .text("")
+        .action { (x, c) => c.copy(t_wid = x) }
+      opt[Int]("w_len")
+        .text("")
+        .action { (x, c) => c.copy(w_len = x) }
+      help("help").text("prints this usage text")
+    }
+
+    parser.parse(args, default_params) match {
+      case Some(params) => run(params)
+      case None => System.exit(1)
+    }
+  }
+
+  def run(p: Params): Unit = {
+    val conf = new SparkConf()
+      .setAppName(s"tianyi-final user-visit-count")
+      .set("spark.hadoop.validateOutputSpecs", "false")
+    if (tianyi.is_local) {
+      conf.setMaster("local[4]")
+    }
+    val sc = new SparkContext(conf)
+
+    val f_len = 1
+    val vvd = RawPoint.read(sc, p.vvd_fp, Int.MaxValue)
+    val cdd = CandidateGenerator.run(vvd, p.t_wid, p.w_len).map((_, Array.fill[Double](f_len)(0.0)))
+    val fs = RawPoint.filter(vvd, p.t_wid, p.w_len).map {
+      p =>
+        (p.uid, p.vcnt)
+    }.reduceByKey(_+_).map {
+      e =>
+        (e._1, Array(e._2.toDouble))
+    }
+
+    val fs_all = cdd.leftOuterJoin(fs).map {
+      e =>
+        (e._1, e._2._2.getOrElse(e._2._1))
+    }
+
+    FeatureOpts.save(fs, p.out_fp)
+  }
+}
