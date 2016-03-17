210d200bccf7bc29486f00f94ac9d0585c105d2d
HouJP <houjp1992@gmail.com>
2016-03-17 23:14:23 +0800

    update

--- bin/run.sh
+++ bin/run.sh
@@ -7,16 +7,30 @@
 #! /bin/bash
 
 
+function run() {
+	local t_wid=$1
+	local w_len=$2
+	local fs_name=$3
+
+	./fs_user-active-count.sh ${t_wid} ${w_len}
+	./fs_user-visit-count.sh ${t_wid} ${w_len}
+	./fs_user-vt-first.sh ${t_wid} ${w_len}
+	./fs_user-vt-last.sh ${t_wid} ${w_len}
+	./fs_merge.sh ${t_wid} ${w_len} ${fs_name}
+	./fs_mylibsvm.sh ${t_wid} ${w_len} ${fs_name}
+
+	hdfs dfs -getmerge 	${HDFS_PROJECT_PT}/data/fs/mylibsvm_${fs_name}_${t_wid}_${w_len}.txt \
+						${LOCAL_PROJECT_PT}/data/fs/mylibsvm_${fs_name}_${t_wid}_${w_len}.txt
+}
+
+t_wid=6
+w_len=5
+fs_name=user-active-count_user-visit-count_user-vt-first_user-vt-last
+
+run $t_wid $w_len $fs_name
+
 t_wid=7
 w_len=5
 fs_name=user-active-count_user-visit-count_user-vt-first_user-vt-last
 
-./fs_user-active-count.sh ${t_wid} ${w_len}
-./fs_user-visit-count.sh ${t_wid} ${w_len}
-./fs_user-vt-first.sh ${t_wid} ${w_len}
-./fs_user-vt-last.sh ${t_wid} ${w_len}
-./fs_merge.sh ${t_wid} ${w_len} ${fs_name}
-./fs_mylibsvm.sh ${t_wid} ${w_len} ${fs_name}
-
-hdfs dfs -getmerge ${HDFS_PROJECT_PT}/data/fs/mylibsvm_${fs_name}_${t_wid}_${w_len}.txt \
-					${LOCAL_PROJECT_PT}/data/fs/mylibsvm_${fs_name}_${t_wid}_${w_len}.txt
+run $t_wid $w_len $fs_name
