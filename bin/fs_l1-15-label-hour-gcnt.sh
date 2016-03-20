f3fe6d472b153d850747be46cff29d6a382bbb9b
HouJP <houjp1992@gmail.com>
2016-03-20 20:39:36 +0800

    update

---
+++ bin/fs_l1-15-label-hour-gcnt.sh
@@ -0,0 +1,42 @@
+#########################################################################
+# File Name: fs_user-active-count.sh
+# Author: HouJP
+# mail: houjp1992@gmail.com
+# Created Time: Wed Mar 16 20:47:49 2016
+#########################################################################
+#! /bin/bash
+
+PATH_PRE="`pwd`"
+PATH_NOW="`dirname $0`"
+cd "${PATH_NOW}"
+source ../conf/shell.conf
+cd "${PATH_PRE}"
+
+if [ 2 -ne $# ]; then
+	echo "[ERROR] Usage: cmd <t_wid> <w_len>"
+	exit 255
+fi
+
+t_wid=$1
+w_len=$2
+vvd_fp=${HDFS_PROJECT_PT}/data/raw/video-visit-data.txt
+out_fp=${HDFS_PROJECT_PT}/data/fs/l1-15-label-hour-gcnt_${t_wid}_${w_len}.txt
+
+
+hdfs dfs -rmr $out_fp
+
+class=com.houjp.tianyi.classification.feature.L115LabelHourGCount
+
+spark-submit \
+	--class $class \
+	${LOCAL_JAR_FP} \
+	--vvd_fp ${vvd_fp} \
+	--out_fp ${out_fp} \
+	--t_wid ${t_wid} \
+	--w_len ${w_len}
+
+if [ 0 -eq $? ]; then
+	echo "[INFO] $class success."
+else
+	echo "[ERROR] $class meet error!"
+fi
--- bin/run.sh
+++ bin/run.sh
@@ -103,6 +103,15 @@ function generate_libsvm() {
 	# 	echo "[INFO] fs_${f_name} ${t_wid} ${w_len} success."
 	# fi
 
+	f_name=l1-15-label-hour-gcnt
+	sh fs_${f_name}.sh ${t_wid} ${w_len}
+	if [ 0 -ne $? ]; then
+		echo "[ERROR] fs_${f_name} ${t_wid} ${w_len} meet error!" 
+		return 255
+	else
+		echo "[INFO] fs_${f_name} ${t_wid} ${w_len} success."
+	fi
+
 	./fs_merge.sh ${t_wid} ${w_len} ${fs_name} 
 	if [ 0 -ne $? ]; then
 		echo "[ERROR] ./fs_merge.sh ${t_wid} ${w_len} ${fs_name} meet error!" 
@@ -134,7 +143,7 @@ function generate_libsvm() {
 function run() {
 	w_len=5
 	#fs_name=l1-label-number_l1-label-visit_l1-label-visit-count_l1-label-visit-rate
-	fs_name=s1-fs_l1-label-number_l1-label-day-gcnt
+	fs_name=s1-fs_l1-label-number_l1-15-label-hour-gcnt
 
 	t_wid_train=6
 	generate_libsvm $t_wid_train $w_len $fs_name
