7c725e41da5b936957602ef0fc1f3e965342e5e5
HouJP <houjp1992@gmail.com>
2016-04-25 13:23:14 +0800

    update split

--- ijcai-16-on-spark/src/main/scala/com/houjp/ijcai16/preprocess/Split.scala
+++ ijcai-16-on-spark/src/main/scala/com/houjp/ijcai16/preprocess/Split.scala
@@ -43,21 +43,19 @@ object Split {
     val taobao = Taobao.load(sc, params.data_pt + "/data-sets/ijcai2016_taobao")
 
     // random split user by 1:1
-    val Array(u1_rdd, u2_rdd) = koubei_train.map(_.user_id).distinct().randomSplit(Array(0.5, 0.5))
-    val u1 = u1_rdd.collect()
-    val u2 = u2_rdd.collect()
+    val Array(u1, u2) = koubei_train.map(_.user_id).distinct().randomSplit(Array(0.5, 0.5))
 
     // split koubei_train
-    val koubei_train_1 = koubei_train.filter(e => u1.contains(e.user_id))
-    val koubei_train_2 = koubei_train.filter(e => u2.contains(e.user_id))
+    val koubei_train_1 = koubei_train.map(e => (e.user_id, e)).join(u1.map(e => (e, 1))).map(_._2._1)
+    val koubei_train_2 = koubei_train.map(e => (e.user_id, e)).join(u2.map(e => (e, 1))).map(_._2._1)
 
     // split taobao
-    val taobao_1 = taobao.filter(e => u1.contains(e.user_id))
-    val taobao_2 = taobao.filter(e => u2.contains(e.user_id))
+    val taobao_1 = taobao.map(e => (e.user_id, e)).join(u1.map(e => (e, 1))).map(_._2._1)
+    val taobao_2 = taobao.map(e => (e.user_id, e)).join(u2.map(e => (e, 1))).map(_._2._1)
 
     // save on disk
-    u1_rdd.saveAsTextFile(params.data_pt + "/data-sets-split/user_p1")
-    u2_rdd.saveAsTextFile(params.data_pt + "/data-sets-split/user_p2")
+    u1.saveAsTextFile(params.data_pt + "/data-sets-split/user_p1")
+    u2.saveAsTextFile(params.data_pt + "/data-sets-split/user_p2")
     koubei_train_1.saveAsTextFile(params.data_pt + "/data-sets-split/ijcai2016_koubei_train_p1")
     koubei_train_2.saveAsTextFile(params.data_pt + "/data-sets-split/ijcai2016_koubei_train_p2")
     taobao_1.saveAsTextFile(params.data_pt + "/data-sets-split/ijcai2016_taobao_p1")
@@ -67,8 +65,8 @@ object Split {
     println(s"[INFO] n(taobao)=${taobao.count}")
 
     println(s"[INFO] n(user)=${koubei_train.map(_.user_id).distinct().count}")
-    println(s"[INFO] n(u1)=${u1_rdd.count}")
-    println(s"[INFO] n(u2)=${u2_rdd.count}")
+    println(s"[INFO] n(u1)=${u1.count}")
+    println(s"[INFO] n(u2)=${u2.count}")
 
     println(s"[INFO] n(koubei_train_1)=${koubei_train_1.count}")
     println(s"[INFO] n(koubei_train_2)=${koubei_train_2.count}")
